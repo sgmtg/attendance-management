8963c2ce0dee9c3ddda5cefa67f31025d8e8db29
sgmtg <shanbenyan3@gmail.com>
2024-02-15 18:48:44 +0900

    fix build error

--- src/AdminPage.tsx
+++ src/AdminPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAuth } from './AuthContext';
 import { db } from './firebase/Firebase'; // Firebaseの初期化設定をインポート
 import { doc, getDoc } from 'firebase/firestore';
